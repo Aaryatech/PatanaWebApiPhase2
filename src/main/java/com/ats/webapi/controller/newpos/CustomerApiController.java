84ec6014c1d8cadb368514f27fa8b0a9cfe72b06
ashay <ashay@ashay-Inspiron-5520>
2021-02-04 18:43:50 +0530

    /Akshay

--- src/main/java/com/ats/webapi/controller/newpos/CustomerApiController.java
+++ src/main/java/com/ats/webapi/controller/newpos/CustomerApiController.java
@@ -4,35 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ats.webapi.model.newpos.Customer;
 import com.ats.webapi.repository.newpos.CustomerRepository;
 
-
-
 @RestController
 public class CustomerApiController {
 
 	@Autowired
 	CustomerRepository custRepo;
-	
-	
-	@RequestMapping(value="/getAllCustomer",method=RequestMethod.GET)
-	public @ResponseBody List<Customer> getAllCustomer(){
-		List<Customer> customerList=new ArrayList<Customer>();
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@RequestMapping(value = "/getAllCustomer", method = RequestMethod.GET)
+	public @ResponseBody List<Customer> getAllCustomer() {
+		List<Customer> customerList = new ArrayList<Customer>();
 		try {
-			customerList=custRepo.getAllCustomer();
+			customerList = custRepo.getAllCustomer();
+
+			String sql = "SELECT * FROM CUSTOMER WHERE ID = ?";
+
 		} catch (Exception e) {
-			customerList=new ArrayList<Customer>();
+			customerList = new ArrayList<Customer>();
 			// TODO: handle exception
 			e.printStackTrace();
 		}
 		return customerList;
 	}
-	
-	
+
+	@RequestMapping(value = "/getAllCustomerForPos", method = RequestMethod.POST)
+	public @ResponseBody List<Customer> getAllCustomerForPos(@RequestParam("frId") int frId) {
+		List<Customer> customerList = new ArrayList<Customer>();
+		try {
+			System.out.println(frId);
+			customerList = custRepo.getAllCustomer(frId);
+
+		} catch (Exception e) {
+			customerList = new ArrayList<Customer>();
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return customerList;
+	}
+
 }
--- src/main/java/com/ats/webapi/repository/newpos/CustomerRepository.java
+++ src/main/java/com/ats/webapi/repository/newpos/CustomerRepository.java
@@ -4,13 +4,44 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ats.webapi.model.newpos.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
-	
-	@Query(value="SELECT uuid() AS id, a.* FROM (\n" + 
-			"SELECT user_name AS user_name,user_gst_no AS gst_no ,user_phone AS phone_no FROM t_sell_bill_header GROUP BY user_phone UNION ALL SELECT sp_cust_name AS user_name,cust_gst_no AS gst_no,sp_cust_mob_no AS phone_no FROM t_sp_cake  GROUP BY  sp_cust_mob_no) a WHERE a.user_name != '' GROUP BY a.phone_no ORDER BY  a.user_name ASC",nativeQuery=true)
+
+	@Query(value = "SELECT uuid() AS id, a.* FROM (\n"
+			+ "SELECT user_name AS user_name,user_gst_no AS gst_no ,user_phone AS phone_no FROM t_sell_bill_header GROUP BY user_phone UNION ALL SELECT sp_cust_name AS user_name,cust_gst_no AS gst_no,sp_cust_mob_no AS phone_no FROM t_sp_cake  GROUP BY  sp_cust_mob_no) a WHERE a.user_name != '' GROUP BY a.phone_no ORDER BY  a.user_name ASC", nativeQuery = true)
 	List<Customer> getAllCustomer();
+
+	@Query(value = "SELECT\n"
+			+ "     uuid() AS id,\n"
+			+ "     a.* \n"
+			+ " FROM\n"
+			+ "     ( SELECT\n"
+			+ "         user_name AS user_name,\n"
+			+ "         user_gst_no AS gst_no ,\n"
+			+ "         user_phone AS phone_no \n"
+			+ "     FROM\n"
+			+ "         t_sell_bill_header where fr_id=:frId\n"
+			+ "     GROUP BY\n"
+			+ "         user_phone \n"
+			+ "     UNION\n"
+			+ "     ALL SELECT\n"
+			+ "         sp_cust_name AS user_name,\n"
+			+ "         cust_gst_no AS gst_no,\n"
+			+ "         sp_cust_mob_no AS phone_no \n"
+			+ "     FROM\n"
+			+ "         t_sp_cake  where fr_id=:frId\n"
+			+ "     GROUP BY\n"
+			+ "         sp_cust_mob_no\n"
+			+ " ) a \n"
+			+ "WHERE\n"
+			+ " a.user_name != '' \n"
+			+ "GROUP BY\n"
+			+ " a.phone_no \n"
+			+ "ORDER BY\n"
+			+ " a.user_name ASC", nativeQuery = true) 
+	List<Customer> getAllCustomer(@Param("frId") int frId);
 
 }
